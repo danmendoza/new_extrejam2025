32e449b62a2aacd429ec455becdc0808e368c20a
alanrose <mynameisalanrose@gmail.com>
2025-11-02 10:56:02 +0100

    RENAMES
    RENAMES

--- Assets/Dani/PlayerControllerTMP.cs
+++ Assets/Dani/PlayerControllerTMP.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerTMP : MonoBehaviour
 {
     Rigidbody2D m_rigidbody2D = null;
     BoxCollider2D m_boxCollider2D = null;
